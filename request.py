26fa3b831191884c42e3cac51faffe704395948e
Kaua.Nichioka <73563366+kinguim1000@users.noreply.github.com>
2022-03-10 15:59:41 -0300

    Atualizar coments

--- request.py
+++ request.py
@@ -2,13 +2,13 @@ import requests
 requisicao =  requests.get('https://hackersdamatrix.herokuapp.com/at3json')
 requisicaojson = requisicao.json()
 
-numuser = 0#valor padrão
+numuser = 0#valor padrao
 #+1 por pular o 0
 for toDo in requisicaojson:#descobrir quantidade de usuarios
     if int(toDo['userId']) > numuser:
         numuser = int(toDo['userId'])
         maximo = [0]*(numuser+1)
-        lideranca = [0]*(numuser+1)#liderança não é mais que a quantidade de usuaris
+        lideranca = [0]*(numuser+1)#lideranca nao e mais que a quantidade de usuaris
 
 for toDo in requisicaojson:
            
@@ -29,7 +29,7 @@ for toDo in requisicaojson:
 
                 
 
-for remover in range (numuser+1):#remover quem não é o "vencedor"
+for remover in range (numuser+1):#remover quem nao e o "vencedor"
     verigualdade = set(lideranca)
     contemduplicado = len(lideranca) != len(verigualdade)
     if(contemduplicado):
